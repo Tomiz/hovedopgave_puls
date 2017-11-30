89b37172c5a41be3e7c692459994630332375ccc
Thomas Kruse Markussen <thomas-tanja@live.dk>
2017-11-30 10:34:48 +0100

    Add project files.

---
+++ Heart_Rate_Fake_Pulse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Heart_Rate_Fake_Pulse
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
